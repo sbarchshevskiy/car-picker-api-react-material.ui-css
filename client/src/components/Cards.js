306556049530ce4baf4540ac21c9c76f71cb3725
sergey <sbarchshevskiy@gmail.com>
2021-04-28 18:02:37 -0400

    refactor and simplify code

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -1,7 +1,7 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-import {CircularProgress, IconButton, Typography} from "@material-ui/core";
+import { IconButton, Typography } from "@material-ui/core";
 import { Close } from "@material-ui/icons";
 import CardActions from "@material-ui/core/CardActions";
 import Button from "@material-ui/core/Button";
@@ -10,11 +10,9 @@ import Modal from "@material-ui/core/Modal";
 import Backdrop from "@material-ui/core/Backdrop";
 import Fade from "@material-ui/core/Fade";
 import axios from "axios";
-import "./Cards.css"
 require("dotenv").config();
+
 const url = process.env.REACT_APP_API_URL;
-
-
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -47,14 +45,10 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-
 export default function Cards({ model, id, make }) {
-
   // Modal state off by default
   const [open, setOpen] = useState(false);
   const [models, setModel] = useState([]);
-  const [loading, setLoading] = useState(false)
-
 
   // Modal popup on open and close
   const handleOpen = () => {
@@ -64,103 +58,82 @@ export default function Cards({ model, id, make }) {
     setOpen(false);
   };
 
-
   useEffect(() => {
     axios
       .get(`${url}${make}/vehicleType/car?format=json`)
       .then((res) => {
         //loads car model
-        console.log('Model: ',res.data["Results"][0].Model_Name);
-        setModel(res.data["Results"])
-        setLoading(true)
+        setModel(res.data["Results"]);
       })
-
       .catch((err) => {
         console.log("ERROR: ", err);
       });
   }, [url]);
 
-
   const classes = useStyles();
-  if (models) {
-    return (
-      <div>
-        <Card elevation={3}>
-          <CardContent>
-            <Typography variant="h5" component="h2">
-              {make}
-              <IconButton
-                onClick={() => {
-                  console.log("close");
-                }}
-              >
-                <Close />
-              </IconButton>
-            </Typography>
 
-            <Typography className={classes.pos} color="textSecondary">
-              model: {model}
-            </Typography>
-            <Typography variant="body2" component="p">
-              id: {id}
-              <br />
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small" onClick={handleOpen}>
-              See complete list
-            </Button>
-          </CardActions>
-        </Card>
-        <Modal
-          aria-labelledby="transition-modal-title"
-          aria-describedby="transition-modal-description"
-          className={classes.modal}
-          open={open}
-          onClose={handleClose}
-          closeAfterTransition
-          BackdropComponent={Backdrop}
-          scrollable={true}
-          BackdropProps={{
-            timeout: 500,
-          }}
-        >
-          <Fade in={open}>
-              <div className={classes.paper}>
-                <div className="modal-body">
-                  {models
-                    .filter((res) => res.Make_Name.toLowerCase())
-                    .map((data, key) => {
-
-                        return (
-                          <div>
-                            <h2 id="transition-modal-title">
-                              {data.Model_Name}
-                            </h2>
-                            <p id="transition-modal-description">
-                              Model id: {data.Model_ID}
-                   
-                            </p>
-                          </div>
-                        )
-
-                    })}
-                </div>
-              </div>
-          </Fade>
-
-        </Modal>
-      </div>
-    );
-
-  } else {
-    return (
-      <div>
-        <Backdrop className={classes.backdrop} open>
-          <CircularProgress color="inherit" />
-        </Backdrop>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <Card elevation={3}>
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            {make}
+            <IconButton
+              onClick={() => {
+                console.log("close");
+              }}
+            >
+              <Close />
+            </IconButton>
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            model: {model}
+          </Typography>
+          <Typography variant="body2" component="p">
+            id: {id}
+            <br />
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" onClick={handleOpen}>
+            See complete list
+          </Button>
+        </CardActions>
+      </Card>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        scrollable={true}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <div className="modal-body">
+              {models
+                .filter((res) => res.Make_Name.toLowerCase())
+                .map((data, key) => {
+                  return (
+                    <div>
+                      <div>
+                        <h2 id="transition-modal-title">{data.Model_Name}</h2>
+                        <p id="transition-modal-description">
+                          Model id: {data.Model_ID}
+                        </p>
+                      </div>
+                    </div>
+                  );
+                })}
+            </div>
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+  );
 }
-
--- client/src/components/FetchData.js
+++ client/src/components/FetchData.js
@@ -1,52 +1,26 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Cards from "./Cards";
-import { CircularProgress } from "@material-ui/core";
-import Backdrop from "@material-ui/core/Backdrop";
-import "./Cards.css"
-
 
 require("dotenv").config();
 const url = process.env.REACT_APP_API_URL;
 
-
 export default function FetchData() {
   const [makes, setMakes] = useState([]);
   const [brand, setBrand] = useState([]);
-  const [loading, setLoading] = useState(false);
-
 
   useEffect(() => {
-    //alternative can be using Promise.all
     axios
       .get(`${url}${brand}/vehicleType/car?format=json`)
       .then((res) => {
         //loads car maker
-        console.log('Maker: ',res.data["Results"][0].Make_Name);
-        setMakes(res.data["Results"])
-        setLoading(true)
+        console.log("Maker: ", res.data["Results"][0].Make_Name);
+        setMakes(res.data["Results"]);
       })
       .catch((err) => {
         console.log("ERROR: ", err);
       });
-  }, [url, brand]);
-
-  // autocomplete function
-  // const [searchMatch, setSearchMatch] = useState([]);
-  //
-  // const autoComplete = (input) => {
-  //   let matches = makes.filter((make) => {
-  //     //regex gi modifier is a case insensitive search
-  //     const regex = new RegExp(`${input}, "gi"`);
-  //     console.log('matches:', make.Make_Name)
-  //     return make.Make_Name.match(regex);
-  //   });
-  //   setSearchMatch(matches);
-  // }
-  // inside jsx
-  // autoComplete(setBrand(event.target.value))
-
-  if (makes) {
+  }, [brand]);
     return (
       <div>
         <body>
@@ -56,45 +30,27 @@ export default function FetchData() {
             placeholder="Search vehicle makes"
             value={brand}
             onChange={(event) => {
-              setBrand(event.target.value)
+              setBrand(event.target.value);
             }}
           />
-          {/*{searchMatch && searchMatch.map((data, key) => (*/}
-          {/*  <div key={key} style={{marginTop: "5px"}}>*/}
-          {/*    <Card style={{width: "50%"}} out={data.Model_Name}/>*/}
-          {/*    {data.Model_Name}*/}
-          {/*  </div>*/}
-          {/*))}*/}
         </body>
 
-
         <div className="container">
-
           {makes
             .filter((res) => res.Make_Name.toLowerCase() === brand)
             .map((data, key) => {
-                return (
-                  <div>
-                    <Cards
-                      model={data.Model_Name}
-                      id={data.Model_ID}
-                      make={data.Make_Name}
-                    />
-                  </div>
-                );
+              return (
+                <div>
+                  <Cards
+                    model={data.Model_Name}
+                    id={data.Model_ID}
+                    make={data.Make_Name}
+                  />
+                </div>
+              );
             })}
         </div>
-
-        {/*<div>*/}
-        {/*  <Cards make={makes}/>*/}
-        {/*</div>*/}
       </div>
     );
-  } else {
-    return (
-      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: "80vh"}}>
-        <CircularProgress color="inherit" />
-      </div>
-      )
-  }
+  
 }
